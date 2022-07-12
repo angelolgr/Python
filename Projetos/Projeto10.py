307387664b1c2147bfa78d9beb7f7d9f7a0dfbf1
angelolgr <angelogiaretta@gmail.com>
2022-07-11 22:21:14 -0300

    Create Projeto10.py

---
+++ Projetos/Projeto10.py
@@ -0,0 +1,7 @@
+from importlib import import_module
+import math
+from multiprocessing.spawn import import_main_path
+math
+n=float(input("Dígite um número:"))
+valor=math.ceil(n)
+print(f"Aredondando {n} temos o valor:{valor}")
